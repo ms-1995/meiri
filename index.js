4adb72cff3b13c9698a30c535eea966ea3246454
ms-1995 <zsq1450566763@gmail.com>
2017-07-13 16:20:27 +0800

    修改index.js

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ var getData = require('./mongodb/data.js');
 var webCrawler = require('./crawler/webCrawler.js');
 
 webCrawler();
+// function s(){
+// 	webCrawler();
+// 	console.log(1);
+// }
+// setInterval(s,10000);
